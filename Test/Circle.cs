e8f8ebe89b689d5fb470733432249fea8c09561d
Jorge Sepulveda <jorluiseptor@gmail.com>
2016-05-16 18:26:55 -0400

    First commit.

---
+++ Test/Circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COIS240
+{
+    class Circle
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private string _name = "bar";
+        private double _radius;
+
+        /// <summary>
+        /// The distance from the center of the circle.
+        /// </summary>
+        public double radius {
+            get 
+            {
+                return _radius;
+            }
+            set
+            {
+                _radius = value; 
+            }
+        }
+
+        
+        /// <summary>
+        /// Calculates and returns area of this circle.
+        /// </summary>
+        /// <returns>Area of circle.</returns>
+        public double getArea()
+        {
+            return System.Math.PI * _radius;
+        }
+
+
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COIS240
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test circle
+            Circle myCircle = new Circle();
+            myCircle.radius = 1;
+            myCircle.getArea();
+            Console.WriteLine("hello");
+            Console.WriteLine(myCircle.getArea());
+            Console.ReadLine();
+        }
+    }
+}
