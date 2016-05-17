ee662d701e2dd8b9048efd8e4687e2c17d6e7961
Jorge Sepulveda <jorluiseptor@gmail.com>
2016-05-16 22:23:07 -0400

    Add Spaceship project.

---
+++ Spaceship/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spaceship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Spaceship s1 = new Spaceship();
+            s1.name = "Battlestar Gallactica";
+            Console.WriteLine(s1.state);
+            Console.WriteLine(s1.name);
+
+            Spaceship s2 = new Spaceship();
+            s2.name = "Millenium Falcon";
+            s2.TurnOn();
+
+
+            Console.ReadLine();
+
+        }
+    }
+
+  
+}
---
+++ Spaceship/Spaceship.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spaceship
+{
+    class Spaceship
+    {
+
+        #region Attributes
+            private string _name;
+            private string _state = "Offline";
+        #endregion
+
+        #region Properties
+            /// <summary>
+            /// Name of the ship.
+            /// </summary>
+            public string name
+            {
+                get
+                {
+                    return _name;
+                }
+                set
+                {
+                    _name = value;
+                }
+            }
+            /// <summary>
+            /// Current state of the ship. 
+            /// </summary>
+            public string state
+            {
+                get
+                {
+                    return _state;
+                }
+            }
+        #endregion
+
+
+        #region Methods
+        /// <summary>
+        /// Make the ship take off. 
+        /// </summary>
+        public void TakeOff()
+        {
+            //Ship is now flying.
+            _state = "Flying";
+        }
+
+        /// <summary>
+        /// Turns the ship online. The ship must be online before it can do other commands.
+        /// </summary>
+        public void TurnOn()
+        {
+            //Ship is now on. 
+            _state = "Online"; 
+        }
+        #endregion
+
+    }
+}
