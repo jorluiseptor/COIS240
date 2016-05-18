ccd7f1c03548e03824fcea73ad5aeecc2defef74
Jorge Sepulveda <jorluiseptor@gmail.com>
2016-05-17 23:42:51 -0400

    Add projects from class of May 17, 2016.

---
+++ FortuneCookie/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortuneCookie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(inbeddifier("You will grow old "));
+            Console.ReadLine();
+        }
+
+        static string inbeddifier(string prediction)
+        {
+            return prediction + " in bed!";
+        }
+    }
+}
---
+++ Repaso/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double alto;
+            double ancho;
+            double area; 
+
+            alto = 5;
+            ancho = 10;
+            area = add(alto, ancho); // alto * ancho
+        
+
+            Console.WriteLine(area);
+            Console.ReadLine();
+        }
+
+        static double add(double v1,double v2)
+        {
+            double producto = v1 + v2;
+            return producto; 
+        }
+    }
+}
