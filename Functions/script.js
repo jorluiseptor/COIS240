d8537dbd4c974c096b380d884025ff955b61302d
Jorge Sepulveda <jorluiseptor@gmail.com>
2018-06-29 13:12:35 -0400

    Add basic template.

---
+++ Functions/script.js
@@ -0,0 +1,5 @@
+//document.body.innerHTML = "foo!";
+document.getElementById("output").innerHTML = "foo bar";
+function Calculate() {
+    document.getElementById("output").innerHTML = "hi";
+}
---
+++ Functions/script.ts
@@ -0,0 +1,8 @@
+//document.body.innerHTML = "foo!";
+
+document.getElementById("output").innerHTML = "foo bar";
+
+
+function Calculate(): void {
+    document.getElementById("output").innerHTML = "hi";
+}
